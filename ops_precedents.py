bae2d54dee705390ecf5da3c7865f665868c6b5e
diheinkalpage <dihein.kalpage@gmail.com>
2026-02-24 20:05:55 +1100

    Add files via upload

---
+++ ops_precedents.py
@@ -0,0 +1,15 @@
+v = 45
+w = 9
+x = 36
+y = 2
+z = (v+w) * x / y
+print(f"The value of the equation (v+w) * x / y is {z}")
+
+name = "Joe"
+age = 25
+
+if name == "Joe" or name == "John" and age >= 25 :
+    print("Hi")
+
+else :
+    print("bye")
---
+++ ops_precedents1.py
@@ -0,0 +1,8 @@
+num = int(input("Please enter a number a number(numerator) "))
+numd = int(input("Please enter a second number (denominator) "))
+
+if num%numd == 0 :
+    print(f"{num} is divisible by {numd}")
+
+else :
+    print(f"{num} is not divisible by {numd}")
