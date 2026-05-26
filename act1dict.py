5af0c2fbe9ef2c2d4c0a72c5859a2c861cdf6ef6
diheinkalpage <dihein.kalpage@gmail.com>
2026-05-26 18:46:03 +1000

    Add files via upload

---
+++ act1dict.py
@@ -0,0 +1,7 @@
+mydict = {'name': 'Dk', 'age': 11, 'school': 'SFX'} 
+print(mydict)
+print(mydict['name'])
+mydict['school'] = 'SPC'
+print(mydict)
+mydict['gender'] = 'Male'
+print(mydict)
---
+++ act3_countrycode.py
@@ -0,0 +1,3 @@
+con_code = {'SL': '0092', 'India': '0091', 'Australia': '00152'}
+print(con_code.get('SL', 'Not found'))
+print(con_code.get('Japan', 'Not found'))
